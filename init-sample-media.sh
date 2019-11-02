1d4ade17c06bacbbe05755032e9d207d10d946c4
Austin Moore <amoore1337@gmail.com>
2019-11-01 20:32:46 -0400

    Better test dir structure

--- init-sample-media.sh
+++ init-sample-media.sh
@@ -5,6 +5,6 @@
 
 if [ ! -d "./media" ]; then
   echo "Create new media content..."
-  mkdir -p ./media/movies ./media/tv_shows
-  touch ./media/tv_shows/test_tv.txt ./media/movies/test_movie.txt
+  mkdir -p ./media/movies ./media/movies/test_movie ./media/tv_shows ./media/tv_shows/test_show ./media/tv_shows/test_show/season_01
+  touch ./media/tv_shows/test_show/season_01/test_tv.txt ./media/movies/test_movie/test_movie.txt
 fi
